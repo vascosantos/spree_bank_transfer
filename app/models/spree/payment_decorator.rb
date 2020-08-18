13e795a5d4e89fd0bb12d384e069d14c61416c47
Vasco Santos <vasco.santos@inlogic.pt>
2020-08-18 17:14:32 +0100

    updated to work with zeitwerk

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,6 +1,9 @@
 module Spree::PaymentDecorator
   attr_accessor :validate_bank_details
-  validates :bank_name, :account_no, :transaction_reference_no, :deposited_on, :presence => true, :if => :validate_bank_details
+
+  def self.prepended(base)
+    base.validates :bank_name, :account_no, :transaction_reference_no, :deposited_on, :presence => true, :if => :validate_bank_details
+  end
 
   scope :from_bank_transfer, -> { joins(:payment_method).where(:spree_payment_methods => { :type => 'Spree::PaymentMethod::BankTransfer' }) }
 
